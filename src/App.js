15a3b5e047e53805acc60e37a367a05f6ee93bda
100SeongJun <94966290+100SeongJun@users.noreply.github.com>
2022-07-15 10:05:01 +0900

    app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  useEffect(() => {
+    fetch("http://localhost:8080/deploy").then((response) => {
+      response.text().then((data) => {
+        setmsg(data);
+      });
+    });
+  });
+  const [msg, setmsg] = useState("");
+  console.log(msg);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{msg}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
