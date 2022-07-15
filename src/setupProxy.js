2b523bc6905dab5851eb7fa9222182b4bceaa9a2
100SeongJun <94966290+100SeongJun@users.noreply.github.com>
2022-07-15 11:17:24 +0900

    commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = (app) => {
-  app.use(
-    "",
-    createProxyMiddleware({
-      target: "http://3.94.85.13:8080",
-      changeOrigin: true,
-    })
-  );
-};
+// module.exports = (app) => {
+//   app.use(
+//     "",
+//     createProxyMiddleware({
+//       target: "http://3.94.85.13:8080",
+//       changeOrigin: true,
+//     })
+//   );
+// };
